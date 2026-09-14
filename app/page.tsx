e1386eb50dd767d497b704e1d5d08aba9f6c871c
Carlos Verdugo <carlos.versa99@gmail.com>
2026-09-14 13:52:41 -0700

    Fetching users on index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 
-const page = () => {
+async function fetchUsers() {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  return data.data;
+}
+
+const IndexPage = async () => {
+  const users = await fetchUsers();
+
+  console.log(users)
+
   return (
     <div>
       <h1>Index Page</h1>
+      <div>
+        { JSON.stringify(users) }
+      </div>
     </div>
   )
 }
 
-export default page;
+export default IndexPage;
