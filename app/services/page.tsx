66b44c8b4284f589e2067c256ab576ea58646a3c
Carlos Verdugo <carlos.versa99@gmail.com>
2026-09-14 13:00:32 -0700

    Adding service and about pages

---
+++ app/services/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServicesPage = () => {
+  return (
+    <div>
+        <h1>Services Page</h1>
+        <p>This is the services page.</p>
+    </div>
+  )
+}
+
+export default ServicesPage
